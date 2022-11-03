cf24661ffb2dc730325ab74e3861e6f7cd25b7cf
Ryan Chen <ryanchen979@gmail.com>
2022-11-03 10:09:02 -0400

    started

---
+++ index.js
@@ -0,0 +1,7 @@
+const DOMSelector = {
+  button: document.getElementById("btn"),
+  text: document.getElementById("text"),
+  box: document.getElementById("big-black-box"),
+  list: document.querySelectorAll(".point"),
+};
+console.log(DOMSelector.text);
