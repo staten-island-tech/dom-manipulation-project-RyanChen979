15bdd69d4b34ca279b60911e356692422087b908
Ryan Chen <ryanchen979@gmail.com>
2022-11-03 10:16:36 -0400

    idk man

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ const DOMSelector = {
   list: document.querySelectorAll(".point"),
 };
 console.log(DOMSelector.text);
+
+function styling(background, content) {
+  background.style.backgroundColor = "purple";
+  content.style.fontsize = "2rem";
+}
