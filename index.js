c1ad05e7b02da6ad994dd40720b1b07540c91df0
Ryan Chen <ryanchen979@gmail.com>
2022-11-16 10:14:02 -0500

    bruh

--- index.js
+++ index.js
@@ -5,21 +5,21 @@ const DOMSelector = {
   input: document.querySelectorAll(".text-box"),
 };
 
-console.log(DOMSelector.submit, DOMSelector.button, DOMSelector.box);
+console.log(DOMSelector.submit);
 DOMSelector.submit.addEventListener("submit", function (e) {
   e.preventDefault();
+
   let input = DOMSelector.input;
+
   let sport = {};
   sport.name = input[0].value;
   sport.team = input[1].value;
-  const listing (sport) [
-    sport.array.forEach(element => {
-      console.log(element.name, element.team);
-    })
-  ]
-  
-
-  console.log(input);
-  return input;
+  sport.player = input[2].value;
+  sport.img = input[3].value;
+  console.log(input[0].value);
+  console.log(input[1].value);
+  console.log(input[2].value);
+  console.log(input[3].value);
 });
-DOMSelector.input.insertAdjacentHTML("beforebegin", `${input}`);
+
+//outputs (card)
