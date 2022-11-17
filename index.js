01a0292deb6745bd1e55067a0df1010a9917e300
Ryan Chen <ryanchen979@gmail.com>
2022-11-17 10:13:52 -0500

    uhh

--- index.js
+++ index.js
@@ -1,25 +1,40 @@
 const DOMSelector = {
   submit: document.getElementById("form"),
   button: document.getElementById("button"),
-  box: document.getElementById("info-box"),
+  box: document.getElementById("display"), //not rtight
   input: document.querySelectorAll(".text-box"),
 };
 
 console.log(DOMSelector.submit);
-DOMSelector.submit.addEventListener("submit", function (e) {
-  e.preventDefault();
 
-  let input = DOMSelector.input;
+function input() {
+  let input = DOMSelector.input.value;
 
   let sport = {};
   sport.name = input[0].value;
   sport.team = input[1].value;
   sport.player = input[2].value;
   sport.img = input[3].value;
-  console.log(input[0].value);
-  console.log(input[1].value);
-  console.log(input[2].value);
-  console.log(input[3].value);
-});
 
-//outputs (card)
+  return sport;
+}
+
+function card() {
+  DOMSelector.box.insertAdjacentHTML(
+    "beforeend",
+    `<h2>${input[0].value}</h2>
+      <br>
+      <h3>${input[1].value}</h3>
+      <br>
+      <h3>${input[2].value}</h3>`
+  );
+}
+
+DOMSelector.submit.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  console.log(input());
+
+  console.log(card());
+  card();
+});
