a6d768cdf17dcdc68f7b8313db3a5e186a9750a5
Ryan Chen <ryanchen979@gmail.com>
2022-11-21 10:16:29 -0500

    bruh the remove

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const DOMSelector = {
   button: document.getElementById("button"),
   box: document.querySelector(".display"),
   input: document.querySelectorAll(".text-box"),
-  removeBtn: document.getElementsByClassName(".remove-btn"),
+  removeBtn: document.querySelector(".remove-btn"),
   name: document.getElementById("game"),
   character: document.getElementById("character"),
   img: document.getElementById("url"),
@@ -11,74 +11,65 @@ const DOMSelector = {
   age: document.getElementById("age"),
 };
 
-function userInput() {
-  let input = DOMSelector.input;
-
-  let game = {
-    name: input[0].value,
-    character: input[1].value,
-    img: input[2].value,
-    gender: input[3].value,
-    age: input[4].value,
-  };
-  return game;
-}
-
 console.log(DOMSelector.input, DOMSelector.submit);
 
-function insertHTML() {
-  const info = {
-    title: DOMSelector.name.value,
-    context: DOMSelector.character.value,
-    img: DOMSelector.img.value,
-    gender: DOMSelector.gender.value,
-    age: DOMSelector.age.value,
-  };
-  console.log(info);
-  return info;
-}
+// function insertHTML() {
+//   const info = {
+//     title: DOMSelector.name.value,
+//     context: DOMSelector.character.value,
+//     img: DOMSelector.img.value,
+//     gender: DOMSelector.gender.value,
+//     age: DOMSelector.age.value,
+//   };
+//   console.log(info);
+//   return info;
+// }
 
-const card = Object.create(
-  `<div class="display-card">
-      <h2 class="display-name">${info.title}</h2>
-      <h3 class="display-character">${info.context}</h3>
-      <img class="display-img" src="${info.img}" />
-      <h4 class="display-gender">${info.gender}</h4>
-      <h4 class="display-age">${info.age}</h4>
-      <button class="remove btn">Remove Card</button>
-    </div>`
-);
+// const card = Object.create(
+//   `<div class="display-card">
+//       <h2 class="display-name">${info.title}</h2>
+//       <h3 class="display-character">${info.context}</h3>
+//       <img class="display-img" src="${info.img}" />
+//       <h4 class="display-gender">${info.gender}</h4>
+//       <h4 class="display-age">${info.age}</h4>
+//       <button class="remove btn">Remove Card</button>
+//     </div>`
+// );
+
+function userInput() {
+  let input = DOMSelector.input;
+  let game = {};
+  game.name = input[0].value;
+  game.character = input[1].value;
+  game.img = input[2].value;
+  game.gender = input[3].value;
+  game.age = input[4].value;
+  return game;
+}
 
 DOMSelector.submit.addEventListener("submit", function (e) {
   e.preventDefault();
 
-  console.log(userInput());
-  console.log(insertHTML());
+  let game = userInput();
+  console.log(game);
 
-  // console.log(card());
-  // card();
   DOMSelector.box.insertAdjacentHTML(
     "afterbegin",
     `
-    ${card}
-    // <div class="display-card">
-    //    <h2 class="display-name">${DOMSelector.title}</h2>
-    //    <h3 class="display-character">${DOMSelector.context}</h3>
-    //    <img class="display-img" src="${DOMSelector.img}" />
-    //    <h4 class="display-gender">${DOMSelector.gender}</h4>
-    //    <h4 class="display-age">${DOMSelector.age}</h4>
-    //    <button class="remove btn">Remove Card</button>
-    //  </div>`
+      <div class="display-card">
+        <h2 class="display-name">Video Game: ${game.name}</h2>
+        <h3 class="display-character">Game Character: ${game.character}</h3>
+        <img class="display-img" src="${game.img}" />
+        <h4 class="display-gender">Gender: ${game.gender}</h4>
+        <h4 class="display-age">Age: ${game.age}</h4>
+        <button class="remove btn" id="0">Remove Card</button>
+      </div>`
   );
-
-  // remove();
 });
 
-// <h2>${card.name}</h2>
-// <br>
-// <h3>${card.character}</h3>
-// <br>
-// <img class="info-img" src="${card.img}"/>
-// <br>
-// <h4>${card.gender}</h4>
-// <h4>${card.age}</h4>
+function remove() {}
+
+DOMSelector.removeBtn.addEventListener("click", (event) => {
+  const item = document.querySelector("display-card");
+  item.remove();
+});
